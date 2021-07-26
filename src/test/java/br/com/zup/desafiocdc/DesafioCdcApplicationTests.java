19f4e9b8c5f2f347c89a792aed2a3befc48507bc
Pedro <pedro.diniz@zup.com.br>
2021-07-26 11:31:38 -0300

    First commit

---
+++ src/test/java/br/com/zup/desafiocdc/DesafioCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafiocdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
